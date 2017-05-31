d9069a1287ec84ed0b19969a772b0b63e6faa762
Paul Bartlett <paul.bartlett@everyonecounts.com>
2017-05-30 19:31:43 -0700

    Initial commit

---
+++ helloworld.js
@@ -0,0 +1,26 @@
+var myApp = angular.module('helloworld', ['ui.router']);
+
+myApp.config(function($stateProvider) {
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>hello world!</h3>'
+  }
+
+  var aboutState = {
+    name: 'about',
+    url: '/about',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+  var paulState = {
+    name: 'paul',
+    url: '/paul',
+    templateUrl: 'paul.html'
+  }
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+  $stateProvider.state(paulState);
+
+});
